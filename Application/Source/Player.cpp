2aa65e05ae5e19f19f77765b7f2feb3dc9ba970f
ohy99 <ohy99@hotmail.com>
2017-02-14 10:54:20 +0800

    Add files via upload

---
+++ Application/Source/Player.cpp
@@ -0,0 +1,100 @@
+#include "Player.h"
+#include "Application.h"
+#include "Mesh.h"
+#include "MainScene.h"
+
+Player* Player::Instance_ = 0;
+Player::Player() : GameObject("Player") 
+{
+	//int hp_;
+	//Vector3 pos_;
+	//static Player* Instance_;
+	//States state_;
+
+	//Weapon* weapons_[Weapon_types::wt_Count];
+	//Weapon* currentWeapon_;
+}
+
+Player* Player::getInstance() 
+{ 
+	if (Instance_) 
+		return Instance_; 
+	else 
+		return (Instance_ = new Player()); 
+}
+void Player::setPosition(Vector3& pos)
+{
+	pos_ = pos;
+}
+
+
+void Player::update(double dt, Camera* cam)
+{
+
+	std::vector<GameObject*> nearObjs;
+
+	//PreCheck collision
+	//Mesh projected("Projected");
+	//projected.setHb(true, PMesh[MESH_TYPE::Body]->TempMin, PMesh[MESH_TYPE::Body]->TempMax, PMesh[MESH_TYPE::Body]->pos, PMesh[MESH_TYPE::Body]->dir);
+
+	static float moveSPD = 1.5f;
+
+	if (Application::IsKeyPressed('W'))
+	{
+		/*bool bam = false;
+		projected.pos.x += (dir_.x * dt * moveSPD);
+		projected.pos.z += (dir_.z * dt * moveSPD);
+
+		for (size_t i = 0; i < nearObjs.size(); i++)
+		{
+			if (projected.isCollide(nearObjs[i]->CollisionMesh_) == true)
+			{
+				bam = true;
+				break;
+			}
+		}
+
+		if (bam == false)
+		{
+			pos_.x += (dir_.x * dt * moveSPD);
+			pos_.z += (dir_.z * dt * moveSPD);
+		}*/
+	}
+	if (Application::IsKeyPressed('S'))
+	{
+
+	}
+	if (Application::IsKeyPressed('A'))
+	{
+
+	}
+	if (Application::IsKeyPressed('D'))
+	{
+
+	}
+}
+
+void Player::render()
+{
+
+}
+
+
+void Player::getPointedObj(Camera* cam)
+{
+	Position TargetPoint;
+	TargetPoint.Set(cam->getTarget().x, cam->getTarget().y, cam->getTarget().z);
+	Pointed_Obj = NULL;
+
+
+	for (auto it : MainScene::Game_Objects_)
+	{
+		if (it->CollisionMesh_->isPointInsideAABB(TargetPoint) == true)
+		{
+			Pointed_Obj = it;
+			break;
+		}
+
+	}
+
+}
